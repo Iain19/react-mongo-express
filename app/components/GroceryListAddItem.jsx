7a023712338ba73465d6a88e50fcfae6b9041235
Igor Khudik <js.web23@gmail.com>
2016-01-08 17:37:57 +0200

    Add item functionality added

---
+++ app/components/GroceryListAddItem.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {
+            input: ""
+        };
+    },
+
+    handleInputName: function(e) {
+        this.setState({
+            input: e.target.value
+        })
+    },
+
+    addItem: function(e) {
+        e.preventDefault();
+        console.log('Adding item ', this.state.input);
+    },
+
+    render: function() {
+        return (
+            <div className="grocery-add-item">
+                <form onSubmit={this.addItem}>
+                    <input value={this.state.input} onChange={this.handleInputName}/>
+                    <button>Add item</button> 
+                </form>
+            </div>
+        )
+    }
+})
