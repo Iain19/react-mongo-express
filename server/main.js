0e1bcb43843c34fbee170e2a296cda3416d5005c
Igor Khudik <js.web23@gmail.com>
2016-04-15 23:05:58 +0300

    Server rendering

--- server/main.js
+++ server/main.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var app = new express();
 var parser = require('body-parser');
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var GroceryItem = require('./models/GroceryItem.js');
 
+require('babel-core/register');
 require('./database.js');
 
 app.get('/', function(req, res) {
-    res.render('./../app/index.ejs', {});
+    // res.render('./../app/index.ejs', {});
+    var application = React.createFactory(require('./../app/components/GroceryItemList.jsx'));
+
+    GroceryItem.find(function(error, doc) {
+        var generated = ReactDOMServer.renderToString(application({
+            items: doc
+        }));
+
+        res.render('./../app/index.ejs', {
+            reactOutput: generated
+        });
+    });
 })
 .use(express.static(__dirname + '/../.tmp'))
 .listen(8080);
